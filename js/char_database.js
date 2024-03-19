e477f5bebd0bc6eee92380a12c728740290f9e6f
Toushin0672 <72175689+Toushin0672@users.noreply.github.com>
2024-03-19 12:54:47 +0900

    Add files via upload

---
+++ js/char_database.js
@@ -0,0 +1,643 @@
+char_database =
+[
+    {
+        "Name": "sora",
+        "Country": "6other",
+        "Rarity": "5",
+        "Element": "all",
+        "Weapon": "sword",
+        "Sex": "man"
+    },
+    {
+        "Name": "albedo",
+        "Country": "1mondo",
+        "Rarity": "5",
+        "Element": "stone",
+        "Weapon": "sword",
+        "Sex": "man"
+    },
+    {
+        "Name": "amber",
+        "Country": "1mondo",
+        "Rarity": "4",
+        "Element": "fire",
+        "Weapon": "bow",
+        "Sex": "woman"
+    },
+    {
+        "Name": "barbara",
+        "Country": "1mondo",
+        "Rarity": "4",
+        "Element": "water",
+        "Weapon": "magic",
+        "Sex": "woman"
+    },
+    {
+        "Name": "bennett",
+        "Country": "1mondo",
+        "Rarity": "4",
+        "Element": "fire",
+        "Weapon": "sword",
+        "Sex": "man"
+    },
+    {
+        "Name": "diluc",
+        "Country": "1mondo",
+        "Rarity": "5",
+        "Element": "fire",
+        "Weapon": "largesword",
+        "Sex": "man"
+    },
+    {
+        "Name": "diona",
+        "Country": "1mondo",
+        "Rarity": "4",
+        "Element": "ice",
+        "Weapon": "bow",
+        "Sex": "woman"
+    },
+    {
+        "Name": "eula",
+        "Country": "1mondo",
+        "Rarity": "5",
+        "Element": "ice",
+        "Weapon": "largesword",
+        "Sex": "woman"
+    },
+    {
+        "Name": "fischl",
+        "Country": "1mondo",
+        "Rarity": "4",
+        "Element": "thunder",
+        "Weapon": "bow",
+        "Sex": "woman"
+    },
+    {
+        "Name": "jean",
+        "Country": "1mondo",
+        "Rarity": "5",
+        "Element": "wind",
+        "Weapon": "sword",
+        "Sex": "woman"
+    },
+    {
+        "Name": "kaeya",
+        "Country": "1mondo",
+        "Rarity": "4",
+        "Element": "ice",
+        "Weapon": "sword",
+        "Sex": "man"
+    },
+    {
+        "Name": "klee",
+        "Country": "1mondo",
+        "Rarity": "5",
+        "Element": "fire",
+        "Weapon": "magic",
+        "Sex": "woman"
+    },
+    {
+        "Name": "lisa",
+        "Country": "1mondo",
+        "Rarity": "4",
+        "Element": "thunder",
+        "Weapon": "magic",
+        "Sex": "woman"
+    },
+    {
+        "Name": "mika",
+        "Country": "1mondo",
+        "Rarity": "4",
+        "Element": "ice",
+        "Weapon": "spear",
+        "Sex": "man"
+    },
+    {
+        "Name": "mona",
+        "Country": "1mondo",
+        "Rarity": "5",
+        "Element": "water",
+        "Weapon": "magic",
+        "Sex": "woman"
+    },
+    {
+        "Name": "noelle",
+        "Country": "1mondo",
+        "Rarity": "4",
+        "Element": "stone",
+        "Weapon": "largesword",
+        "Sex": "woman"
+    },
+    {
+        "Name": "razor",
+        "Country": "1mondo",
+        "Rarity": "4",
+        "Element": "thunder",
+        "Weapon": "largesword",
+        "Sex": "man"
+    },
+    {
+        "Name": "rosaria",
+        "Country": "1mondo",
+        "Rarity": "4",
+        "Element": "ice",
+        "Weapon": "spear",
+        "Sex": "woman"
+    },
+    {
+        "Name": "sucrose",
+        "Country": "1mondo",
+        "Rarity": "4",
+        "Element": "wind",
+        "Weapon": "magic",
+        "Sex": "woman"
+    },
+    {
+        "Name": "venti",
+        "Country": "1mondo",
+        "Rarity": "5",
+        "Element": "wind",
+        "Weapon": "bow",
+        "Sex": "man"
+    },
+    {
+        "Name": "baizhu",
+        "Country": "2ri-yue",
+        "Rarity": "5",
+        "Element": "grass",
+        "Weapon": "magic",
+        "Sex": "man"
+    },
+    {
+        "Name": "beidou",
+        "Country": "2ri-yue",
+        "Rarity": "4",
+        "Element": "thunder",
+        "Weapon": "largesword",
+        "Sex": "woman"
+    },
+    {
+        "Name": "chongyun",
+        "Country": "2ri-yue",
+        "Rarity": "4",
+        "Element": "ice",
+        "Weapon": "largesword",
+        "Sex": "man"
+    },
+    {
+        "Name": "gaming",
+        "Country": "2ri-yue",
+        "Rarity": "4",
+        "Element": "fire",
+        "Weapon": "largesword",
+        "Sex": "man"
+    },
+    {
+        "Name": "ganyu",
+        "Country": "2ri-yue",
+        "Rarity": "5",
+        "Element": "ice",
+        "Weapon": "bow",
+        "Sex": "woman"
+    },
+    {
+        "Name": "hutao",
+        "Country": "2ri-yue",
+        "Rarity": "5",
+        "Element": "fire",
+        "Weapon": "spear",
+        "Sex": "woman"
+    },
+    {
+        "Name": "keqing",
+        "Country": "2ri-yue",
+        "Rarity": "5",
+        "Element": "thunder",
+        "Weapon": "sword",
+        "Sex": "woman"
+    },
+    {
+        "Name": "ningguang",
+        "Country": "2ri-yue",
+        "Rarity": "4",
+        "Element": "stone",
+        "Weapon": "magic",
+        "Sex": "woman"
+    },
+    {
+        "Name": "qiqi",
+        "Country": "2ri-yue",
+        "Rarity": "5",
+        "Element": "ice",
+        "Weapon": "sword",
+        "Sex": "woman"
+    },
+    {
+        "Name": "shenhe",
+        "Country": "2ri-yue",
+        "Rarity": "5",
+        "Element": "ice",
+        "Weapon": "spear",
+        "Sex": "woman"
+    },
+    {
+        "Name": "tartaglia",
+        "Country": "2ri-yue",
+        "Rarity": "5",
+        "Element": "water",
+        "Weapon": "bow",
+        "Sex": "man"
+    },
+    {
+        "Name": "xiangling",
+        "Country": "2ri-yue",
+        "Rarity": "4",
+        "Element": "fire",
+        "Weapon": "spear",
+        "Sex": "woman"
+    },
+    {
+        "Name": "xianyun",
+        "Country": "2ri-yue",
+        "Rarity": "5",
+        "Element": "wind",
+        "Weapon": "magic",
+        "Sex": "woman"
+    },
+    {
+        "Name": "xiao",
+        "Country": "2ri-yue",
+        "Rarity": "5",
+        "Element": "wind",
+        "Weapon": "spear",
+        "Sex": "man"
+    },
+    {
+        "Name": "xingqiu",
+        "Country": "2ri-yue",
+        "Rarity": "4",
+        "Element": "water",
+        "Weapon": "sword",
+        "Sex": "man"
+    },
+    {
+        "Name": "xinyan",
+        "Country": "2ri-yue",
+        "Rarity": "4",
+        "Element": "fire",
+        "Weapon": "largesword",
+        "Sex": "woman"
+    },
+    {
+        "Name": "yanfei",
+        "Country": "2ri-yue",
+        "Rarity": "4",
+        "Element": "fire",
+        "Weapon": "magic",
+        "Sex": "woman"
+    },
+    {
+        "Name": "yaoyao",
+        "Country": "2ri-yue",
+        "Rarity": "4",
+        "Element": "grass",
+        "Weapon": "spear",
+        "Sex": "woman"
+    },
+    {
+        "Name": "yelan",
+        "Country": "2ri-yue",
+        "Rarity": "5",
+        "Element": "water",
+        "Weapon": "bow",
+        "Sex": "woman"
+    },
+    {
+        "Name": "yunjin",
+        "Country": "2ri-yue",
+        "Rarity": "4",
+        "Element": "stone",
+        "Weapon": "spear",
+        "Sex": "woman"
+    },
+    {
+        "Name": "zhongli",
+        "Country": "2ri-yue",
+        "Rarity": "5",
+        "Element": "stone",
+        "Weapon": "spear",
+        "Sex": "man"
+    },
+    {
+        "Name": "arataki",
+        "Country": "3inazuma",
+        "Rarity": "5",
+        "Element": "stone",
+        "Weapon": "largesword",
+        "Sex": "man"
+    },
+    {
+        "Name": "ayaka",
+        "Country": "3inazuma",
+        "Rarity": "5",
+        "Element": "ice",
+        "Weapon": "sword",
+        "Sex": "woman"
+    },
+    {
+        "Name": "ayato",
+        "Country": "3inazuma",
+        "Rarity": "5",
+        "Element": "water",
+        "Weapon": "sword",
+        "Sex": "man"
+    },
+    {
+        "Name": "goro",
+        "Country": "3inazuma",
+        "Rarity": "4",
+        "Element": "stone",
+        "Weapon": "bow",
+        "Sex": "man"
+    },
+    {
+        "Name": "heizou",
+        "Country": "3inazuma",
+        "Rarity": "4",
+        "Element": "wind",
+        "Weapon": "magic",
+        "Sex": "man"
+    },
+    {
+        "Name": "kazuha",
+        "Country": "3inazuma",
+        "Rarity": "5",
+        "Element": "wind",
+        "Weapon": "sword",
+        "Sex": "man"
+    },
+    {
+        "Name": "kirara",
+        "Country": "3inazuma",
+        "Rarity": "4",
+        "Element": "grass",
+        "Weapon": "sword",
+        "Sex": "woman"
+    },
+    {
+        "Name": "kokomi",
+        "Country": "3inazuma",
+        "Rarity": "5",
+        "Element": "water",
+        "Weapon": "magic",
+        "Sex": "woman"
+    },
+    {
+        "Name": "kujyou",
+        "Country": "3inazuma",
+        "Rarity": "4",
+        "Element": "thunder",
+        "Weapon": "bow",
+        "Sex": "woman"
+    },
+    {
+        "Name": "raiden",
+        "Country": "3inazuma",
+        "Rarity": "5",
+        "Element": "thunder",
+        "Weapon": "spear",
+        "Sex": "woman"
+    },
+    {
+        "Name": "sayu",
+        "Country": "3inazuma",
+        "Rarity": "4",
+        "Element": "wind",
+        "Weapon": "largesword",
+        "Sex": "woman"
+    },
+    {
+        "Name": "shinobu",
+        "Country": "3inazuma",
+        "Rarity": "4",
+        "Element": "thunder",
+        "Weapon": "sword",
+        "Sex": "woman"
+    },
+    {
+        "Name": "toma",
+        "Country": "3inazuma",
+        "Rarity": "4",
+        "Element": "fire",
+        "Weapon": "spear",
+        "Sex": "man"
+    },
+    {
+        "Name": "yaemiko",
+        "Country": "3inazuma",
+        "Rarity": "5",
+        "Element": "thunder",
+        "Weapon": "magic",
+        "Sex": "woman"
+    },
+    {
+        "Name": "yoimiya",
+        "Country": "3inazuma",
+        "Rarity": "5",
+        "Element": "fire",
+        "Weapon": "bow",
+        "Sex": "woman"
+    },
+    {
+        "Name": "aruhaizenn",
+        "Country": "4sume-ru",
+        "Rarity": "5",
+        "Element": "grass",
+        "Weapon": "sword",
+        "Sex": "man"
+    },
+    {
+        "Name": "dhishia",
+        "Country": "4sume-ru",
+        "Rarity": "5",
+        "Element": "fire",
+        "Weapon": "largesword",
+        "Sex": "woman"
+    },
+    {
+        "Name": "dori",
+        "Country": "4sume-ru",
+        "Rarity": "4",
+        "Element": "thunder",
+        "Weapon": "largesword",
+        "Sex": "woman"
+    },
+    {
+        "Name": "faruzan",
+        "Country": "4sume-ru",
+        "Rarity": "4",
+        "Element": "wind",
+        "Weapon": "bow",
+        "Sex": "woman"
+    },
+    {
+        "Name": "hourousya",
+        "Country": "4sume-ru",
+        "Rarity": "5",
+        "Element": "wind",
+        "Weapon": "magic",
+        "Sex": "man"
+    },
+    {
+        "Name": "kave",
+        "Country": "4sume-ru",
+        "Rarity": "4",
+        "Element": "grass",
+        "Weapon": "largesword",
+        "Sex": "man"
+    },
+    {
+        "Name": "korei",
+        "Country": "4sume-ru",
+        "Rarity": "4",
+        "Element": "grass",
+        "Weapon": "bow",
+        "Sex": "woman"
+    },
+    {
+        "Name": "kyandisu",
+        "Country": "4sume-ru",
+        "Rarity": "4",
+        "Element": "water",
+        "Weapon": "spear",
+        "Sex": "woman"
+    },
+    {
+        "Name": "nahida",
+        "Country": "4sume-ru",
+        "Rarity": "5",
+        "Element": "grass",
+        "Weapon": "magic",
+        "Sex": "woman"
+    },
+    {
+        "Name": "nilirou",
+        "Country": "4sume-ru",
+        "Rarity": "5",
+        "Element": "water",
+        "Weapon": "sword",
+        "Sex": "woman"
+    },
+    {
+        "Name": "reira",
+        "Country": "4sume-ru",
+        "Rarity": "4",
+        "Element": "ice",
+        "Weapon": "sword",
+        "Sex": "woman"
+    },
+    {
+        "Name": "seno",
+        "Country": "4sume-ru",
+        "Rarity": "5",
+        "Element": "thunder",
+        "Weapon": "spear",
+        "Sex": "man"
+    },
+    {
+        "Name": "thinari",
+        "Country": "4sume-ru",
+        "Rarity": "5",
+        "Element": "grass",
+        "Weapon": "bow",
+        "Sex": "man"
+    },
+    {
+        "Name": "chiori",
+        "Country": "5fonte-nu",
+        "Rarity": "5",
+        "Element": "stone",
+        "Weapon": "sword",
+        "Sex": "woman"
+    },
+    {
+        "Name": "huremine",
+        "Country": "5fonte-nu",
+        "Rarity": "4",
+        "Element": "ice",
+        "Weapon": "largesword",
+        "Sex": "man"
+    },
+    {
+        "Name": "hurina",
+        "Country": "5fonte-nu",
+        "Rarity": "5",
+        "Element": "water",
+        "Weapon": "sword",
+        "Sex": "woman"
+    },
+    {
+        "Name": "navia",
+        "Country": "5fonte-nu",
+        "Rarity": "5",
+        "Element": "stone",
+        "Weapon": "largesword",
+        "Sex": "woman"
+    },
+    {
+        "Name": "nuviretto",
+        "Country": "5fonte-nu",
+        "Rarity": "5",
+        "Element": "water",
+        "Weapon": "magic",
+        "Sex": "man"
+    },
+    {
+        "Name": "rine",
+        "Country": "5fonte-nu",
+        "Rarity": "5",
+        "Element": "fire",
+        "Weapon": "bow",
+        "Sex": "man"
+    },
+    {
+        "Name": "rinetto",
+        "Country": "5fonte-nu",
+        "Rarity": "4",
+        "Element": "wind",
+        "Weapon": "sword",
+        "Sex": "woman"
+    },
+    {
+        "Name": "riosesuri",
+        "Country": "5fonte-nu",
+        "Rarity": "5",
+        "Element": "ice",
+        "Weapon": "magic",
+        "Sex": "man"
+    },
+    {
+        "Name": "shuvuruzu",
+        "Country": "5fonte-nu",
+        "Rarity": "4",
+        "Element": "fire",
+        "Weapon": "spear",
+        "Sex": "woman"
+    },
+    {
+        "Name": "syarurotto",
+        "Country": "5fonte-nu",
+        "Rarity": "4",
+        "Element": "ice",
+        "Weapon": "magic",
+        "Sex": "woman"
+    },
+    {
+        "Name": "aloy",
+        "Country": "6other",
+        "Rarity": "5",
+        "Element": "ice",
+        "Weapon": "bow",
+        "Sex": "woman"
+    }
+]
--- js/index.js
+++ js/index.js
@@ -1,109 +1,32 @@
-//キャラクターの連想配列
-var sora = {Name:'sora', Country:'6other'};
-var albedo = {Name:'albedo', Country:'1mondo'};
-var amber = {Name:'amber', Country:'1mondo'};
-var barbara = {Name:'barbara', Country:'1mondo'};
-var bennett = {Name:'bennett', Country:'1mondo'};
-var diluc = {Name:'diluc', Country:'1mondo'};
-var diona = {Name:'diona', Country:'1mondo'};
-var eula = {Name:'eula', Country:'1mondo'};
-var fischl = {Name:'fischl', Country:'1mondo'};
-var jean = {Name:'jean', Country:'1mondo'};
-var kaeya = {Name:'kaeya', Country:'1mondo'};
-var klee = {Name:'klee', Country:'1mondo'};
-var lisa = {Name:'lisa', Country:'1mondo'};
-var mika = {Name:'mika', Country:'1mondo'};
-var mona = {Name:'mona', Country:'1mondo'};
-var noelle = {Name:'noelle', Country:'1mondo'};
-var razor = {Name:'razor', Country:'1mondo'};
-var rosaria = {Name:'rosaria', Country:'1mondo'};
-var sucrose = {Name:'sucrose', Country:'1mondo'};
-var venti = {Name:'venti', Country:'1mondo'};
-var baizhu = {Name:'baizhu', Country:'2ri-yue'};
-var beidou = {Name:'beidou', Country:'2ri-yue'};
-var chongyun = {Name:'chongyun', Country:'2ri-yue'};
-var gaming = {Name:'gaming', Country:'2ri-yue'};
-var ganyu = {Name:'ganyu', Country:'2ri-yue'};
-var hutao = {Name:'hutao', Country:'2ri-yue'};
-var keqing = {Name:'keqing', Country:'2ri-yue'};
-var ningguang = {Name:'ningguang', Country:'2ri-yue'};
-var qiqi = {Name:'qiqi', Country:'2ri-yue'};
-var shenhe = {Name:'shenhe', Country:'2ri-yue'};
-var tartaglia = {Name:'tartaglia', Country:'2ri-yue'};
-var xiangling = {Name:'xiangling', Country:'2ri-yue'};
-var xianyun = {Name:'xianyun', Country:'2ri-yue'};
-var xiao = {Name:'xiao', Country:'2ri-yue'};
-var xingqiu = {Name:'xingqiu', Country:'2ri-yue'};
-var xinyan = {Name:'xinyan', Country:'2ri-yue'};
-var yanfei = {Name:'yanfei', Country:'2ri-yue'};
-var yaoyao = {Name:'yaoyao', Country:'2ri-yue'};
-var yelan = {Name:'yelan', Country:'2ri-yue'};
-var yunjin = {Name:'yunjin', Country:'2ri-yue'};
-var zhongli = {Name:'zhongli', Country:'2ri-yue'};
-var arataki = {Name:'arataki', Country:'3inazuma'};
-var ayaka = {Name:'ayaka', Country:'3inazuma'};
-var ayato = {Name:'ayato', Country:'3inazuma'};
-var goro = {Name:'goro', Country:'3inazuma'};
-var heizou = {Name:'heizou', Country:'3inazuma'};
-var kazuha = {Name:'kazuha', Country:'3inazuma'};
-var kirara = {Name:'kirara', Country:'3inazuma'};
-var kokomi = {Name:'kokomi', Country:'3inazuma'};
-var kujyou = {Name:'kujyou', Country:'3inazuma'};
-var raiden = {Name:'raiden', Country:'3inazuma'};
-var sayu = {Name:'sayu', Country:'3inazuma'};
-var shinobu = {Name:'shinobu', Country:'3inazuma'};
-var toma = {Name:'toma', Country:'3inazuma'};
-var yaemiko = {Name:'yaemiko', Country:'3inazuma'};
-var yoimiya = {Name:'yoimiya', Country:'3inazuma'};
-var aruhaizenn = {Name:'aruhaizenn', Country:'4sume-ru'};
-var dhishia = {Name:'dhishia', Country:'4sume-ru'};
-var dori = {Name:'dori', Country:'4sume-ru'};
-var faruzan = {Name:'faruzan', Country:'4sume-ru'};
-var hourousya = {Name:'hourousya', Country:'4sume-ru'};
-var kave = {Name:'kave', Country:'4sume-ru'};
-var korei = {Name:'korei', Country:'4sume-ru'};
-var kyandisu = {Name:'kyandisu', Country:'4sume-ru'};
-var nahida = {Name:'nahida', Country:'4sume-ru'}; 
-var nilirou = {Name:'nilirou', Country:'4sume-ru'};
-var reira = {Name:'reira', Country:'4sume-ru'};
-var seno = {Name:'seno', Country:'4sume-ru'};
-var thinari = {Name:'thinari', Country:'4sume-ru'};
-var chiori = {Name:'chiori', Country:'5fonte-nu'};
-var huremine = {Name:'huremine', Country:'5fonte-nu'};
-var hurina = {Name:'hurina', Country:'5fonte-nu'};
-var navia = {Name:'navia', Country:'5fonte-nu'};
-var nuviretto = {Name:'nuviretto', Country:'5fonte-nu'};
-var rine = {Name:'rine', Country:'5fonte-nu'};
-var rinetto = {Name:'rinetto', Country:'5fonte-nu'};
-var riosesuri = {Name:'riosesuri', Country:'5fonte-nu'};
-var shuvuruzu = {Name:'shuvuruzu', Country:'5fonte-nu'};
-var syarurotto = {Name:'syarurotto', Country:'5fonte-nu'};
-var aloy = {Name:'aloy', Country:'6other'};
-
-var all_char =[
-    sora,albedo,amber,barbara,bennett,diluc,diona,eula,fischl,jean,
-    kaeya,klee,lisa,mika,mona,noelle,razor,rosaria,sucrose,venti,
-    baizhu,beidou,chongyun,gaming,ganyu,hutao,keqing,ningguang,qiqi,shenhe,
-    tartaglia,xiangling,xianyun,xiao,xingqiu,xinyan,yanfei,yaoyao,yelan,yunjin,
-    zhongli,arataki,ayaka,ayato,goro,heizou,kazuha,kirara,kokomi,kujyou,
-    raiden,sayu,shinobu,toma,yaemiko,yoimiya,aruhaizenn,dhishia,dori,faruzan,
-    hourousya,kave,korei,kyandisu,nahida,nilirou,reira,seno,thinari,chiori,
-    huremine,hurina,navia,nuviretto,rine,rinetto,riosesuri,shuvuruzu,syarurotto,aloy
-];
+//全キャラのデータを入れる配列を作成
+const ALL_CHAR = [];
+//別jsファイルのjsonデータを'ALL_CHAR'配列に追加
+for(var i=0; i < char_database.length; i++){
+    ALL_CHAR.push(char_database[i]);
+}
 
 //キャラクター画像表示
-for(var i=0; i < all_char.length; i++){
+for(var i=0; i < ALL_CHAR.length; i++){
     const label = document.createElement('label');
     label.setAttribute('for', 'checkbox' + i);
     
     const checkbox = document.createElement('input');
     checkbox.setAttribute('type', 'checkbox');
     checkbox.setAttribute('id', 'checkbox' + i);
-    checkbox.setAttribute('name', all_char[i]['Name']);
+    checkbox.setAttribute('name', ALL_CHAR[i]['Name']);
     checkbox.style.display = 'none';
 
     const image = document.createElement('img');
-    image.setAttribute('src', './image/char_icon/' + all_char[i]['Country'] + '/' + all_char[i]['Name'] + '.png');
+    image.setAttribute('src', './image/char_icon/' + ALL_CHAR[i]['Country'] + '/' + ALL_CHAR[i]['Name'] + '.png');
+    switch(ALL_CHAR[i]['Rarity']){
+        case '4':
+            back_color = '#8457B2';
+            break;
+        case '5':
+            back_color = '#B18A42';
+            break;
+    }
+    image.setAttribute('style', 'background-color: ' + back_color + ';');
 
     label.appendChild(checkbox);
     label.appendChild(image);
@@ -111,68 +34,126 @@ for(var i=0; i < all_char.length; i++){
     document.getElementById('character_list').appendChild(label);
 }
 
-//全キャラクター選択・解除
-function all_select(bool){
-    for(var i=0; i < all_char.length; i++){
-        if(bool == 'true'){
-            document.getElementById('checkbox'+ i).checked = true;
-        }else if(bool == 'false'){
-            document.getElementById('checkbox'+ i).checked = false;
+//ハンバーガーメニューの開閉
+document.querySelector('.hamburger').addEventListener('click', function(){
+    this.classList.toggle('active');
+    document.querySelector('.slide-menu').classList.toggle('active');
+  });
+
+/* 関数群 */
+//キャラクターの属性に合わせてアクティブ化又は全選択、全解除
+function char_select_active(val, num){
+    switch(num){
+        //全キャラクターの選択・解除
+        case 0:
+            if(val === 'true'){
+                var bool = true;
+            }else {
+                var bool = false;
+            }
+            for(var i=0; i < ALL_CHAR.length; i++){
+                document.getElementById('checkbox'+ i).checked = bool;
+            }
+        break;
+        //元素毎
+        case 1:
+            for(var i=0; i < ALL_CHAR.length; i++){
+                var char_name = document.getElementById('checkbox'+ i).getAttribute('name');
+                var data = getstate(char_name);
+                if(data['Element'] == val){
+                    document.getElementById('checkbox'+ i).checked = true;
+                }
+            }
+        break;
+        //武器毎
+        case 2:
+            for(var i=0; i < ALL_CHAR.length; i++){
+                var char_name = document.getElementById('checkbox'+ i).getAttribute('name');
+                var data = getstate(char_name);
+                if(data['Weapon'] == val){
+                    document.getElementById('checkbox'+ i).checked = true;
+                }
+            }
+        break;
+        //国毎
+        case 3:
+            for(var i=0; i < ALL_CHAR.length; i++){
+                var char_name = document.getElementById('checkbox'+ i).getAttribute('name');
+                var data = getstate(char_name);
+                if(data['Country'] == val){
+                    document.getElementById('checkbox'+ i).checked = true;
+                }
+            }
+        break;
+        //レアリティ
+        case 4:
+            for(var i=0; i < ALL_CHAR.length; i++){
+                var char_name = document.getElementById('checkbox'+ i).getAttribute('name');
+                var data = getstate(char_name);
+                if(data['Rarity'] == val){
+                    document.getElementById('checkbox'+ i).checked = true;
+                }
+            }
+        break;
+        //性別
+        case 5:
+            for(var i=0; i < ALL_CHAR.length; i++){
+                var char_name = document.getElementById('checkbox'+ i).getAttribute('name');
+                var data = getstate(char_name);
+                if(data['Sex'] == val){
+                    document.getElementById('checkbox'+ i).checked = true;
+                }
+            }
+        break;
+    }
+}
+
+//キャラクターの名前を渡して連想配列を取得
+function getstate(name){
+    for(var i=0; i < ALL_CHAR.length; i++){
+        if(ALL_CHAR[i]['Name'] == name){
+            return ALL_CHAR[i];
         }
     }
 }
 
-//アクティブキャラのみ抽選
-function select(){
+//抽選機構
+function char_drow(){
+    //アクティブ状態のキャラをすべて配列に突っ込む
     var select_box = [];
-    for(var i=0; i < all_char.length; i++){
-        if(document.getElementById('checkbox'+ i).checked == true){
-            select_box.push(document.getElementById('checkbox'+ i).getAttribute('name'));
+    for(var i=0; i<ALL_CHAR.length; i++){
+        if(document.getElementById('checkbox'+i).checked == true){
+            select_box.push(document.getElementById('checkbox'+i).getAttribute('name'));
         }
     }
+
     if(select_box.length == 0){
+        select_member_clear();
         alert('キャラクターを選択してください。');
     }
 
-    if(select_box.length > 8){
-        for(var i = 0; i < 8; i++){
+    for(var i=0; i < 8; i++){
+        if(select_box.length != 0){
             var select_num = Math.floor(Math.random() *(select_box.length));
             var select_char = select_box[select_num];
-            var getindex = search_index(select_char);
-            document.getElementById('icon' + i).innerHTML = "<img src='./image/char_icon/" + getindex[1] + "/" + getindex[0] + ".png'>";
+            var getData = getstate(select_char);
+            document.getElementById('icon' + i).innerHTML = "<img src='./image/char_icon/" + getData['Country'] + "/" + getData['Name'] + ".png'>";
             document.getElementsByName(select_char)[0].checked = false;
             select_box.splice(select_num, 1);
-        }
-
-        if(select_box.length < 8){
-            select_member_clear();
-            for(var i = 0; 0 < select_box.length; i++){
-                var select_num = Math.floor(Math.random() *(select_box.length));
-                var select_char = select_box[select_num];
-                var getindex = search_index(select_char);
-                document.getElementById('icon' + i).innerHTML = "<img src='./image/char_icon/" + getindex[1] + "/" + getindex[0] + ".png'>";
-                document.getElementsByName(select_char)[0].checked = false;
-                select_box.splice(select_num, 1);
+        }else if(select_box.length == 0){
+            for(i+1; i < 8; i++){
+                document.getElementById('icon' + i).innerHTML = '';
             }
-        }
-    }else if(select_box.length <= 8){
-        for(var i = 0; 0 < select_box.length; i++){
-            var select_num = Math.floor(Math.random() *(select_box.length));
-            var select_char = select_box[select_num];
-            var getindex = search_index(select_char);
-            document.getElementById('icon' + i).innerHTML = "<img src='./image/char_icon/" + getindex[1] + "/" + getindex[0] + ".png'>";
-            document.getElementsByName(select_char)[0].checked = false;
-            select_box.splice(select_num, 1);
         }
     }
 }
 
-//all_charからselect_charで選ばれたキャラの連想配列を取得
+//ALL_CHARからselect_charで選ばれたキャラの連想配列を取得
 function search_index(select_char){
-    for(var i=0; i < all_char.length; i++){
-        if(all_char[i]['Name'] == select_char){
-            var char_name = all_char[i].Name;
-            var char_country = all_char[i].Country;
+    for(var i=0; i < ALL_CHAR.length; i++){
+        if(ALL_CHAR[i]['Name'] == select_char){
+            var char_name = ALL_CHAR[i].Name;
+            var char_country = ALL_CHAR[i].Country;
             return [char_name, char_country];
         }
     }
